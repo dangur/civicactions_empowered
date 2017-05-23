ea748f8aca298385a370c8a97c6af1e8add24046
Dan Gurin <dan@dangur.com>
2017-05-23 13:27:15 -0400

    makes block configurable

--- src/Plugin/Block/CivicActionsEmpoweredBlock.php
+++ src/Plugin/Block/CivicActionsEmpoweredBlock.php
@@ -3,6 +3,8 @@
 namespace Drupal\civicactions_empowered\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'CivicActionsEmpoweredBlock' block.
@@ -12,22 +14,68 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("CivicActions Empowered block"),
  * )
  */
-class CivicActionsEmpoweredBlock extends BlockBase {
+class CivicActionsEmpoweredBlock extends BlockBase implements BlockPluginInterface {
 
   /**
    * {@inheritdoc}
    */
   public function defaultConfiguration() {
-    return ['label_display' => FALSE];
+    $default_config = \Drupal::config('civicactions_empowered.settings');
+    return array(
+      'civic_actions_empowered_block' => $default_config->get('empowered.civicactions'),
+    );
+  }
+
+  /**
+   * {@inherhitdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+    $default_config = \Drupal::config('civicactions_empowered.settings');
+
+
+    $form['display_text'] = array(
+      '#type' => 'textfield',
+      '#format' => 'restricted_html',
+      '#title' => $this->t('CivicActions Empowered block text'),
+      '#description' => $this->t('Modify the text as you wish'),
+      '#default_value' => isset($config['display_text']) ? $config['display_text'] : $default_config,
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate($form, FormStateInterface $form_state) {
+    if (isset($this->configuration['display_text'])) {
+      drupal_set_message("CivicActions Empowered block value set to \"{$this->configuration['display_text']}\"");
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['display_text'] = $values['display_text'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['civic_actions_empowered_block']['#markup'] = '<a href="https://civicactions.com">CivicActions</a> Empowered';
-
+    $config = $this->getConfiguration();
+    if (!empty($config['display_text'])) {
+      $dt = $config['display_text'];
+    }
+    else {
+      $dt = t('<a href="https://civicactions.com">CivicActions</a> Empowered');
+    }
+    $build['civic_actions_empowered_block']['#markup'] = $dt;
     return $build;
   }
 
